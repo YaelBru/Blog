5307f5286a7d5051f59184534409b290f5f7b05e
YaelBru <51443210+YaelBru@users.noreply.github.com>
2020-10-05 20:04:56 +0300

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,14 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: "AIzaSyB2U_oCilWoE-g7U30ypmHFeA_0hE9L2Ag",
-    authDomain: "yb-blog-4c214.firebaseapp.com",
-    databaseURL: "https://yb-blog-4c214.firebaseio.com",
-    projectId: "yb-blog-4c214",
-    storageBucket: "yb-blog-4c214.appspot.com",
-    messagingSenderId: "121629714146",
-    appId: "1:121629714146:web:e00d1d33d670953a82e0bb",
-    measurementId: "G-5JLRQMR2G7"
+   apiKey: "***",
+   authDomain: "yb-blog-4c214.firebaseapp.com",
+   databaseURL: "https://yb-blog-4c214.firebaseio.com",
+   projectId: "***",
+   storageBucket: "***",
+   messagingSenderId: "***",
+   appId: "***",
+   measurementId: "***"
+
   }
 };
